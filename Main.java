0e164dfa656a8490b013605a56735e568b6ca7e7
Sukhu236 <sukhu27274@gmail.com>
2026-08-07 22:41:18 +0530

    c

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
+
+import java.awt.datatransfer.StringSelection;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
         int a = 5;
         int b = 6;
+        String hello = "name";
         System.out.println(a + b);
+        System.out.println(hello);
     }
 }
